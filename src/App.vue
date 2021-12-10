83b87e84c3f8f94db4165216251a4de1f9659f0b
Elvan H <elvanhd@gmail.com>
2021-12-10 20:56:47 +0700

    creating a basic vue app

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Contact List</h2>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">
+      {{ friend.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Aaron",
+          phone: "123-456-7890",
+          email: "aaron@example.com",
+        },
+        {
+          id: 2,
+          name: "Blake",
+          phone: "234-567-8901",
+          email: "blake@example.com",
+        },
+        {
+          id: 3,
+          name: "Caleb",
+          phone: "345-678-9012",
+          email: "caleb@example.com",
+        },
+      ],
+    };
+  },
+};
+</script>
