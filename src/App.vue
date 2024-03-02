47e4a845b0eccbd99962ba4f09bd8e317adf4ed0
Elvan Hidayat <elvanhd@gmail.com>
2024-03-02 22:05:48 +0700

    Refine App and FriendContact component styling
    - Update App.vue with a styled header and global font import
    - Enhance FriendContact.vue to dynamically change button text
    - Apply comprehensive styling for the entire app within App.vue
    - Ensure consistent visual theme across all components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header>
+      <h1>My Friends</h1>
+    </header>
     <ul>
       <friend-contact></friend-contact>
       <friend-contact></friend-contact>
@@ -30,3 +32,63 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Jost', sans-serif;
+}
+body {
+  margin: 0;
+}
+header {
+  width: 90%;
+  max-width: 40rem;
+  margin: 3rem auto;
+  padding: 1rem;
+  border-radius: 10px;
+  color: white;
+  text-align: center;
+  background-color: #58004d;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app li {
+  width: 90%;
+  max-width: 40rem;
+  margin: 1rem auto;
+  padding: 1rem;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+#app h2 {
+  margin: 0 0 1rem 0;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  font-size: 2rem;
+}
+#app button {
+  padding: 0.05rem 1rem;
+  border: 1px solid #ff0077;
+  color: white;
+  font: inherit;
+  cursor: pointer;
+  background-color: #ff0077;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+#app button:hover,
+#app button:active {
+  border-color: #ec3169;
+  background-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
