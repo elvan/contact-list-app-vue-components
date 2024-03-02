741dfcb0e148dec8f810ea0ad51fc2612f5057fb
Elvan Hidayat <elvanhd@gmail.com>
2024-03-02 21:42:32 +0700

    Initialize Vue 3 project structure for Contact List app
    - Set up project configuration with .browserslistrc, .eslintrc.js, and .gitignore files
    - Configure Babel with babel.config.js for modern JavaScript features
    - Initialize package.json with project dependencies and scripts
    - Add public folder with favicon and index.html for the entry point
    - Create initial Vue components in src folder, including App.vue
    - Implement main.js as the entry point for Vue application
    - Define basic project setup and dependencies for Vue 3 development

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'melvin',
+          name: 'Melvin Jenkins',
+          phone: '01234 5678 991',
+          email: 'melvin@localhost.com',
+        },
+        {
+          id: 'jessica',
+          name: 'Jessica Weber',
+          phone: '09876 543 221',
+          email: 'jessica@localhost.com',
+        },
+      ],
+    };
+  },
+};
+</script>
